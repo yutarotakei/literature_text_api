1776e6a0c2e41b642b9f2ee42c060dc02867cb5d
YutaroTakei <yutarotakei@icloud.com>
2022-12-14 19:50:42 +0900

    奇偶の判定をするPost,Getスクリプト

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, jsonify
+from flask import *
  
 app = Flask(__name__)
  
-@app.route('/')
-def hello_world():
-    return jsonify({'message': 'Hello world'})
+@app.route("/", methods=["GET", "POST"])
+def odd_even():
+    if request.method == "GET":
+        return """
+        下に整数を入力してください。奇数か偶数か判定します
+        <form action="/" method="POST">
+        <input name="num"></input>
+        </form>"""
+    else:
+        try:
+            return """
+            {}は{}です！
+            <form action="/" method="POST">
+            <input name="num"></input>
+            </form>""".format(str(request.form["num"]), ["偶数", "奇数"][int(request.form["num"]) % 2])
+        except:
+            return """
+                    有効な数字ではありません！入力しなおしてください。
+                    <form action="/" method="POST">
+                    <input name="num"></input>
+                    </form>"""
  
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888, debug=True)
