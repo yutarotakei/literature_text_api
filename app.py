301bdf5d1159dac786ac9749f751dce8d2c6d0c1
YutaroTakei <yutarotakei@icloud.com>
2022-12-14 18:22:39 +0900

    雛形の作成

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+    return jsonify({'message': 'Hello world'})
+ 
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8888, debug=True)
